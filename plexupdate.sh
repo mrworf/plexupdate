eeabf9d74afe618fafe9fad31f4b6d9825e22631
Alex Malinovich <alexmalinovich@gmail.com>
2016-08-30 23:34:19 -0700

    Fix PATH on Ubuntu systems if dpkg is likely to fail during AUTOINSTALL

--- plexupdate.sh
+++ plexupdate.sh
@@ -548,6 +548,11 @@ if [ ! -z "${PLEXSERVER}" -a "${AUTOINSTALL}" = "yes" ]; then
 fi
 
 if [ "${AUTOINSTALL}" = "yes" ]; then
+	if ! hash ldconfig 2>/dev/null && [ -x /sbin/ldconfig -a "${DISTRO}" = "ubuntu" ]; then
+		export PATH=$PATH:/sbin
+	fi
+	# no elif since DISTRO_INSTALL will produce error output for us
+
 	${DISTRO_INSTALL} "${DOWNLOADDIR}/${FILENAME}"
 fi
 
