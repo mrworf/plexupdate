ac1f861bf13ea38b0ab1f65adbc4141d87ce7d2c
Alex Malinovich <alexmalinovich@gmail.com>
2016-11-29 20:15:59 -0800

    Fix permissions for all files, not just .git

--- plexupdate.sh
+++ plexupdate.sh
@@ -202,9 +202,9 @@ cleanup() {
 	done
 
 	plexupdate_path="$(dirname $0)"
-	# Make sure .git permissions haven't been modified by running as sudo and not sudo on different occasions
+	# Make sure permissions haven't been modified by running as sudo and not sudo on different occasions
 	if [ -d "${plexupdate_path}/.git" ]; then
-		chown -R --reference="${plexupdate_path}" "${plexupdate_path}/.git" &> /dev/null
+		chown -R --reference="${plexupdate_path}" "${plexupdate_path}/" &> /dev/null
 	fi
 }
 trap cleanup EXIT
