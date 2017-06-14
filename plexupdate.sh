34f50ad101ca71b459f5dfd5716712dd01183e8e
n1koo <niko.kurtti@shopify.com>
2017-06-14 08:23:38 +0300

    Remove deprecated source from plexupdate.sh

--- plexupdate.sh
+++ plexupdate.sh
@@ -335,8 +335,6 @@ if [ "${CHECKUPDATE}" = "yes" -a "${AUTOUPDATE}" = "no" ]; then
 fi
 
 if [ "${PUBLIC}" = "no" -a -z "$TOKEN" ]; then
-	TO_SOURCE="$(dirname "$0")/extras/get-plex-token"
-	[ -f "$TO_SOURCE" ] && source $TO_SOURCE
 	if ! getPlexToken; then
 		error "Unable to get Plex token, falling back to public release"
 		PUBLIC="yes"
