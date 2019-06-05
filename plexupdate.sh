b6fc5403f490ad4ea344ffa647c4a6afc8560029
Max <max.yellen@gmail.com>
2019-06-05 14:46:52 -0400

    indent with tabs instead of spaces
    changed spaces to tabs

--- plexupdate.sh
+++ plexupdate.sh
@@ -308,14 +308,14 @@ if [ -z "${DISTRO_INSTALL}" ]; then
 				DISTRO_INSTALL="${REDHAT_INSTALL}"
 			fi
 		elif [ -f /etc/synoinfo.conf ]; then
-		        DISTRO="synology"
-		        if [ "${PUBLIC}" = "yes" ]; then
-			        if [ "${ARCH}" = "x86_64" ]; then
-				    BUILD="linux-ubuntu-x86_64"
+			DISTRO="synology"
+			if [ "${PUBLIC}" = "yes" ]; then
+				if [ "${ARCH}" = "x86_64" ]; then
+					BUILD="linux-ubuntu-x86_64"
 				elif [ "${ARCH}" = "x86" ]; then
-				    BUILD="linux-synology-i686"
+					BUILD="linux-synology-i686"
 				elif [ "${ARCH}" = "armv7" ]; then
-				    BUILD="linux-synology-armv7"
+					BUILD="linux-synology-armv7"
 				fi
 			else
 				BUILD="linux-${ARCH}"
